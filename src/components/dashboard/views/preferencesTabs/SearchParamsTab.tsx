6901b05eb1c50843d3d7a3a6f8c2ddb98c22bef6
Ross Yeager <ryeager12@gmail.com>
2023-07-15 14:53:42 -0700

    wip

---
+++ src/components/dashboard/views/preferencesTabs/SearchParamsTab.tsx
@@ -0,0 +1,44 @@
+import React, { FC } from "react";
+import { DocumentSnapshot } from "firebase/firestore";
+import {
+  Typography,
+  FormGroup,
+  FormControlLabel,
+  Divider,
+} from "@mui/material";
+
+import { ClientConfig } from "types/user";
+import {
+  FirestoreBackedSwitch,
+  FirestoreBackedTextField,
+} from "components/utils/forms";
+
+
+const SearchParamsTab: FC<{
+  userConfigSnapshot: DocumentSnapshot<ClientConfig>;
+}> = ({ userConfigSnapshot }) => {
+  const updatingAnything = !!userConfigSnapshot?.metadata.fromCache;
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Search Term
+      </Typography>
+      <FormGroup>
+        <FirestoreBackedTextField
+          label="Search Term"
+          disabled={updatingAnything}
+          docSnap={userConfigSnapshot!}
+          fieldPath="searchParams.searchString"
+          variant="standard"
+          sx={{ maxWidth: 300 }}
+        />
+      </FormGroup>
+      <Divider sx={{ marginTop: 2, marginBottom: 4 }} />
+      <Typography variant="h6" gutterBottom>
+        Bed & Bath
+      </Typography>
+    </>
+  );
+};
+
+export default SearchParamsTab;
